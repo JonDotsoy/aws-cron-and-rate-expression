64e3641d23357c21eafe8db7d689a348c68264f1
JonDotsoy <JonDotsoy@users.noreply.github.com>
2021-03-22 16:27:33 +0000

    Deploying to gh-pages from @ JonDotsoy/aws-cron-and-rate-expression@fed55badc0bf767109b483c4a4a32ff02c4814a4 🚀

--- _next/static/T2sCukiD6TdJwEaF3lbit/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-225573b78e5784380fa2.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-92a8286c72a4281086bf.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
